5a81f863cfad29221eea73dde4fec53ec77d5d0c
Suhani Sadh <suhanisadh01@gmail.com>
2026-04-05 15:58:43 +0530

    Fix deployment port

--- app.py
+++ app.py
@@ -150,5 +150,8 @@ def get_song(emotion):
     return jsonify(get_song_by_emotion(emotion))
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
